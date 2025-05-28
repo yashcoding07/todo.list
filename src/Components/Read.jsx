75036b3fdee2c3c2d5f02166c3bcf2b9ac42c5d6
Yashparmar0745 <yashparmar0710@gmail.com>
2025-05-28 19:45:55 +0530

    The TO-DO Project Completed

--- src/Components/Read.jsx
+++ src/Components/Read.jsx
@@ -5,8 +5,17 @@ const Read = (props) => {
     const todos = props.todos;
     const settodos = props.settodos;
 
+    const deleteHandler = (id)=>{
+        const filtertodo = todos.filter((todo)=> todo.id != id);
+        settodos(filtertodo);
+    }
+
     const renderTodos = todos.map(todos => {
-        return <li key={todos.id}>{todos.title}</li>
+        return( 
+        <li key={todos.id}>{todos.title}
+        <button onClick={() => deleteHandler(todos.id)}>Task Completed</button>
+        </li>
+    );
     })
 
     return (
